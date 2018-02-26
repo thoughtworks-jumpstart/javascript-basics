2182b3a0b5bbb3c58a49c263f2e9d92b2702e04b
davidtan <davidtan@thoughtworks.com>
2018-02-27 07:45:51 +0800

    Use updated test helper

--- 2-arrays.js
+++ 2-arrays.js
@@ -1,53 +1,45 @@
-var helper = require('./test-helper')
+var helper = require("./test-helper");
 
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Write a JavaScript function which accept a number as input and insert dashes (-) between each two even numbers. (Sample input: 025486, Sample output: 0-254-8-6)
 
 function addDashes(numString) {
-  return '0-254-8-6'
+  return "0-254-8-6";
 }
 
-helper.test(addDashes('025486'), '0-254-8-6') // check that your function works as expected
-// helper.test(dash('111246777'), '1112-4-6777') // uncomment this and run program. if test passes, uncomment subsequent tests one by one
-// helper.test(dash('0021100'), '0-0-2110-0') 
-// helper.test(dash('your test input'), 'your expected output') // uncomment this line and add 2-3 more test cases. Do the same for all remaining functions
+assertEquals(addDashes("025486"), "0-254-8-6"); // check that your function works as expected
+// assertEquals(dash('111246777'), '1112-4-6777') // uncomment this and run program. if test passes, uncomment subsequent tests one by one
+// assertEquals(dash('0021100'), '0-0-2110-0')
+// assertEquals(dash('your test input'), 'your expected output') // uncomment this line and add 2-3 more test cases. Do the same for all remaining functions
 
 /* ---------------------- EXERCISE 2 ---------------------- */
 // Write a Javascript function to find the most frequent item of an array. It should return a string denoting the item and the number of times it occurs in the array. (Sample input: [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3], expected output : 'a (5 times)')
 
-function mostFrequentItem(arr) {
-
-}
+function mostFrequentItem(arr) {}
 
 // uncomment the following test to run it
-// helper.test(mostFrequentItem(['a', 'a', 'b']), 'a')
-// helper.test(mostFrequentItem(['a', 'b', 'b', 'b', 'c', 'a', 'b', 'a', 'b']), 'b')
-// helper.test(mostFrequentItem(['yes', 'yes', 'no', 'no', 'yes']), 'yes')
+// assertEquals(mostFrequentItem(['a', 'a', 'b']), 'a')
+// assertEquals(mostFrequentItem(['a', 'b', 'b', 'b', 'c', 'a', 'b', 'a', 'b']), 'b')
+// assertEquals(mostFrequentItem(['yes', 'yes', 'no', 'no', 'yes']), 'yes')
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 // Write a Javascript function to remove duplicate items from an array (ignore case sensitivity). (Sample input : [1, 'a', 'A', 'b', 2, 2], expected output: [1, 'a', 'b', 2])
-function removeDuplicateItems(arr) {
+function removeDuplicateItems(arr) {}
 
-}
-
-// helper.test(removeDuplicateItems([1, 1, 'a']), [1, 'a'])
-// helper.test(removeDuplicateItems([1, 1, 'a', 'A']), [1, 'a'])
-// helper.test(removeDuplicateItems([1, 'a', 'A', 'b', 'B', 2, 2]), [1, 'a', 'b', 2])
+// assertEquals(removeDuplicateItems([1, 1, 'a']), [1, 'a'])
+// assertEquals(removeDuplicateItems([1, 1, 'a', 'A']), [1, 'a'])
+// assertEquals(removeDuplicateItems([1, 'a', 'A', 'b', 'B', 2, 2]), [1, 'a', 'b', 2])
 
 /* ---------------------- EXERCISE 4 ---------------------- */
 // Write a Javascript function to compute the union of two arrays. the returned array should be sorted in ascending order (Sample input: union([1, 2, 3], [100, 2, 1, 10]), expected output: [1, 2, 3, 10, 100])
-function union() {
+function union() {}
 
-}
-
-// helper.test(union([1, 2], [2, 3]), [1, 2, 3])
-// helper.test(union([1, 2, 3], [100, 2, 1, 10]), [1, 2, 3, 10, 100])
+// assertEquals(union([1, 2], [2, 3]), [1, 2, 3])
+// assertEquals(union([1, 2, 3], [100, 2, 1, 10]), [1, 2, 3, 10, 100])
 
 /* ---------------------- EXERCISE 5 ---------------------- */
 // Write a Javascript function to compute sum of a union. (Hint: Reuse the union() function which you wrote!) (Sample input: intersection([1, 2, 3], [1, 2]), expected output: 3)
-function unionSum() {
-
-}
-// helper.test(unionSum([1, 2, 3], [1, 2]), 3)
-// helper.test(unionSum([1, 2, 3, 4], [2, 3, 4]), 9)
-// helper.test(unionSum([2000, 50, 551, 550, 23], [551, 50, 23]), 624)
+function unionSum() {}
+// assertEquals(unionSum([1, 2, 3], [1, 2]), 3)
+// assertEquals(unionSum([1, 2, 3, 4], [2, 3, 4]), 9)
+// assertEquals(unionSum([2000, 50, 551, 550, 23], [551, 50, 23]), 624)
--- 3-objects.js
+++ 3-objects.js
@@ -1,65 +1,57 @@
-var helper = require('./test-helper')
+var assertEquals = require("./test-helper");
+assertEquals(1, 1);
 
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function to list the properties of a JavaScript object. Go to the editor
 
-function listProperties() {
+function listProperties() {}
 
-}
-
-// helper.test(listProperties({name: 'Tom Hanks'}), 'name')
-// helper.test(listProperties({name: 'Tom Hanks', age: 50, rollno: 12}), 'name age rollno')
+// assertEquals(listProperties({name: 'Tom Hanks'}), 'name')
+// assertEquals(listProperties({name: 'Tom Hanks', age: 50, rollno: 12}), 'name age rollno')
 
 // Sample Output: name,sclass,rollno
 
 /* ---------------------- EXERCISE 2 ---------------------- */
 
-var books = [ 
-    {
-        author: 'Bill Gates',
-        title: 'The Road Ahead',
-        isAvailable: true
-    },
-    {
-        author: 'JRR Tolkkien',
-        title: 'Lord of the Rings',
-        isAvailable: true
-    },
-    {
-        author: 'JK Rowling',
-        title:  'Harry Potter: The Prisoner of Azkaban', 
-        isAvailable: false
-    }
- ]
+var books = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    isAvailable: true
+  },
+  {
+    author: "JRR Tolkkien",
+    title: "Lord of the Rings",
+    isAvailable: true
+  },
+  {
+    author: "JK Rowling",
+    title: "Harry Potter: The Prisoner of Azkaban",
+    isAvailable: false
+  }
+];
 
 // 2.1 Define a function listTitles(booksArray) that takes in an array of book objects and returns an array of titles (strings)
-function listTitles(booksArray) {
+function listTitles(booksArray) {}
 
-}
-
-// helper.test(listTitles(books), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
+// assertEquals(listTitles(books), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
 
 // 2.2 Define a function listAuthors(booksArray) that takes in an array of book objects and returns an array of authors (strings)
 
-function listAuthors(booksArray) {
-    
-}
+function listAuthors(booksArray) {}
 
-// helper.test(listTitles(books), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
+// assertEquals(listTitles(books), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
 
 // 2.3 Define a more general function listValues(booksArray, key) that takes in an array of book objects and returns an array of authors (strings)
 
-function listValues(booksArray, key) {
+function listValues(booksArray, key) {}
 
-}
-
-// helper.test(listValues(books, 'author'), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
-// helper.test(listValues(books, 'title'), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
+// assertEquals(listValues(books, 'author'), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
+// assertEquals(listValues(books, 'title'), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
 
 // 2.4 Define a function getAvailableBooks(booksArray) that returns a list of available books
 
-
-// helper.test(getAvailableBooks(books), ['The Road Ahead', 'Lord of the Rings'])
+// assertEquals(getAvailableBooks(books), ['The Road Ahead', 'Lord of the Rings'])
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 /* 3.1 Define an object named fluffycat with the following properties/methods:
@@ -69,8 +61,7 @@ function listValues(booksArray, key) {
     - introduceSelf() // return 'meow! my name is fluffy'
 */
 
-// helper.test(fluffycat.name, 'fluffy')
-// helper.test(fluffycat.type, 'pet')
-// helper.test(fluffycat.makeSound(), 'meow!')
-// helper.test(fluffycat.introduceSelf(), 'meow! my name is fluffy')
-
+// assertEquals(fluffycat.name, "fluffy");
+// assertEquals(fluffycat.type, "pet");
+// assertEquals(fluffycat.makeSound(), "meow!");
+// assertEquals(fluffycat.introduceSelf(), "meow! my name is fluffy");
