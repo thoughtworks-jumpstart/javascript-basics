e24261e7f13a90d8022d44a46e4ab3eb947e520d
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-11 10:26:42 +0800

    Enable all assertions

--- 2-arrays.js
+++ 2-arrays.js
@@ -1,4 +1,4 @@
-var assertEquals = require("./assert-helper");
+const assertEquals = require("./assert-helper");
 
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function that returns the sum of an array
@@ -6,8 +6,8 @@ function sum(array) {
 
 }
 
-// assertEquals(sum([1, 2]), 3);
-// assertEquals(sum([1, 2, 3, 4]), 10);
+assertEquals(sum([1, 2]), 3);
+assertEquals(sum([1, 2, 3, 4]), 10);
 
 /* ---------------------- EXERCISE 2 ---------------------- */
 // Define a function that returns an array of even numbers
@@ -15,18 +15,18 @@ function evenNumbersOnly(array) {
 
 }
 
-// assertEquals(evenNumbersOnly([1, 2, 3, 4]), [2, 4]);
-// assertEquals(evenNumbersOnly([10, 11, 12]), [10, 12]);
+assertEquals(evenNumbersOnly([1, 2, 3, 4]), [2, 4]);
+assertEquals(evenNumbersOnly([10, 11, 12]), [10, 12]);
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 // Define a function that returns the sum of even numbers in the array
 // hint: reuse the 2 functions that you defined earlier.
 function sumEvenNumbers(array) {
-  
+
 }
 
-// assertEquals(sumEvenNumbers([1, 2, 3, 4]), 6);
-// assertEquals(sumEvenNumbers([10, 11, 12]), 22);
+assertEquals(sumEvenNumbers([1, 2, 3, 4]), 6);
+assertEquals(sumEvenNumbers([10, 11, 12]), 22);
 
 /* ---------------------- BONUS: EXERCISE 4 ---------------------- */
 // Write a Javascript function to compute the union of two arrays. the returned array should be sorted in ascending order (Sample input: union([1, 2, 3], [100, 2, 1, 10]), expected output: [1, 2, 3, 10, 100])
@@ -34,6 +34,6 @@ function sortedUnion(arr1, arr2) {
 
 }
 
-// assertEquals(sortedUnion([1], [2]), [1, 2]);
-// assertEquals(sortedUnion([1, 3], [2]), [1, 2, 3]);
-// assertEquals(sortedUnion([1, 3], [11, 2]), [1, 2, 3, 11]);
+assertEquals(sortedUnion([1], [2]), [1, 2]);
+assertEquals(sortedUnion([1, 3], [2]), [1, 2, 3]);
+assertEquals(sortedUnion([1, 3], [11, 2]), [1, 2, 3, 11]);
--- 3-objects.js
+++ 3-objects.js
@@ -18,32 +18,32 @@ var books = [
   }
 ];
 
-/* 1. Define a function listTitles(booksArray) that takes in an array of book objects 
+/* 1. Define a function listTitles(booksArray) that takes in an array of book objects
        and returns an array of titles (strings) */
 function listTitles(booksArray) {
 
 }
 
-// assertEquals(listTitles(books), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter'])
+assertEquals(listTitles(books), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter'])
 
-/* 2. Define a function listAuthors(booksArray) that takes in an array of book objects 
+/* 2. Define a function listAuthors(booksArray) that takes in an array of book objects
        and returns an array of authors (strings) */
 
 function listAuthors(booksArray) {
 
 }
 
-// assertEquals(listAuthors(books), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
+assertEquals(listAuthors(books), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
 
-/* 3. Define a more general function listValues(booksArray, key) that takes in an array 
+/* 3. Define a more general function listValues(booksArray, key) that takes in an array
        of book objects and returns an array of authors (strings) */
 
 function listValues(booksArray, key) {
 
 }
 
-// assertEquals(listValues(books, 'author'), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
-// assertEquals(listValues(books, 'title'), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter'])
+assertEquals(listValues(books, 'author'), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
+assertEquals(listValues(books, 'title'), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter'])
 
 // 4. Define a function getAvailableBooks(booksArray) that returns a list of available books
 
@@ -51,4 +51,4 @@ function getAvailableBooks(booksArray) {
 
 }
 
-// assertEquals(getAvailableBooks(books), ['The Road Ahead', 'Lord of the Rings'])
+assertEquals(getAvailableBooks(books), ['The Road Ahead', 'Lord of the Rings'])
