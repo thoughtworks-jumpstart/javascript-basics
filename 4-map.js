fdb15e38c766875443197bee8d490cdd71282ec4
davified <davified@gmail.com>
2018-06-12 09:59:18 +0800

    Add map exercises

---
+++ 4-map.js
@@ -0,0 +1,55 @@
+var assertEquals = require("./assert-helper");
+
+/* ---------------------- EXERCISE 1 ---------------------- */
+// define a function that multiplies all elements in an array by 10
+function multiplyBy10(array) {
+
+}
+
+
+// Assertions (do not change)
+assertEquals(multiplyBy10([1, 2]), [10, 20]);
+assertEquals(multiplyBy10([3, 5, 7]), [30, 50, 70]);
+assertEquals(multiplyBy10([-1, 0, 1]), [-10, 0, 10]);
+
+
+/* ---------------------- EXERCISE 2 ---------------------- */
+// define a function that takes in an array objects and returns only an array of strings of the name field
+
+function onlyNames(array) {
+
+}
+
+// Assertions (do not change)
+var input = [
+  {
+    name: "homer",
+    favorite: "donuts"
+  },
+  {
+    name: "marge",
+    favorite: "family"
+  },
+  {
+    name: "lisa",
+    favorite: "school"
+  }
+];
+
+assertEquals(onlyNames(input), ["homer", "marge", "lisa"]);
+
+/* ---------------------- EXERCISE 3 ---------------------- */
+// define a function that removes any non-vowel character from words in an array
+function keepOnlyVowels(array) {
+  
+}
+
+
+// Assertions (do not change)
+assertEquals(keepOnlyVowels(["abc"]), ["a"]);
+assertEquals(keepOnlyVowels(["exceptional"]), ["eeioa"]);
+assertEquals(keepOnlyVowels(["average", "exceptional", "amazing"]), [
+  "aeae",
+  "eeioa",
+  "aai"
+]);
