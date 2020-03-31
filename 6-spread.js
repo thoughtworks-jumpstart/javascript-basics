548031125471d6c5f6d85ba8e2e54e3b4ee95ef5
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-03-31 15:48:02 +0800

    [mabel] Add spread question

---
+++ 6-spread.js
@@ -0,0 +1,9 @@
+var assertEquals = require("./assert-helper");
+
+/* ---------------------- EXERCISE 0 ---------------------- */
+// define a function that returns the smallest number using spread
+function getSmallest(numbers) {
+
+}
+// Assertions (do not change)
+assertEquals(getSmallest([1, 2, 3, 4]), 1);
