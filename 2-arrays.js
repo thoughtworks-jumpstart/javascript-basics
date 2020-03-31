fbd115d83cecd9ff853697da264e87675a74710b
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-03-31 16:41:15 +0800

    [mabel] Add exercise 0 to 2-arrays.js

--- 2-arrays.js
+++ 2-arrays.js
@@ -1,9 +1,21 @@
 const assertEquals = require("./assert-helper");
 
+/* ---------------------- EXERCISE 0 ---------------------- */
+// define a function that returns the smallest number using sort
+
+function getSmallest(numbers) {
+  numbers.sort((a, b) => a - b);
+  return numbers[0];
+}
+
+// Assertions (do not change)
+assertEquals(getSmallest([1, 2, 3, 4]), 1);
+assertEquals(getSmallest([4, 3, 2, 1]), 1);
+
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function that returns the sum of an array
 function sum(array) {
-	return array.reduce((accumulator, number) => accumulator + number, 0);
+  return array.reduce((accumulator, number) => accumulator + number, 0);
 }
 
 // Assertions (do not change)
@@ -13,7 +25,7 @@ assertEquals(sum([1, 2, 3, 4]), 10);
 /* ---------------------- EXERCISE 2 ---------------------- */
 // Define a function that returns an array of even numbers
 function evenNumbersOnly(array) {
-	return array.filter(number => number %2 === 0);
+  return array.filter(number => number % 2 === 0);
 }
 
 // Assertions (do not change)
@@ -24,7 +36,7 @@ assertEquals(evenNumbersOnly([10, 11, 12]), [10, 12]);
 // Define a function that returns the sum of even numbers in the array
 // hint: reuse the 2 functions that you defined earlier.
 function sumEvenNumbers(array) {
-	return sum(evenNumbersOnly(array));
+  return sum(evenNumbersOnly(array));
 }
 
 // Assertions (do not change)
@@ -36,14 +48,14 @@ assertEquals(sumEvenNumbers([10, 11, 12]), 22);
 // array should be sorted in ascending order (Sample input: union([1, 2, 3],
 // [100, 2, 1, 10]), expected output: [1, 2, 3, 10, 100])
 function duplicationFilter(value, index, array) {
-	return array[index - 1] !== value;
+  return array[index - 1] !== value;
 }
 
 function sortedUnion(arr1, arr2) {
-	return arr1
-		.concat(arr2)
-		.sort((a, b) => a - b)
-		.filter(duplicationFilter);
+  return arr1
+    .concat(arr2)
+    .sort((a, b) => a - b)
+    .filter(duplicationFilter);
 }
 
 // Assertions (do not change)
